8b94351a64f92c7db66008f96bb306b230b7bd70
Максим Тутуков <m.tutukov@kokoc.tech>
2022-08-13 22:46:03 +0300

    does client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ALL_USERS, GET_ONE_USER } from './query/user';
+import { CREATE_USER } from './mutations/user';
 
-function App() {
+const App = () => {
+  const { data, loading, refetch } = useQuery(GET_ALL_USERS);
+  const { data: oneUser } = useQuery(GET_ONE_USER, {
+    variables: {
+      id: 1,
+    },
+  });
+  const [newUser] = useMutation(CREATE_USER);
+  const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState(0);
+
+  console.log(oneUser);
+
+  useEffect(() => {
+    if (!loading) {
+      setUsers(data.getAllUsers);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  const addUser = e => {
+    e.preventDefault();
+    newUser({
+      variables: {
+        input: {
+          username,
+          age,
+        },
+      },
+    }).then(({ data }) => {
+      console.log(data);
+      setUsername('');
+      setAge(0);
+    });
+  };
+  const getAll = e => {
+    e.preventDefault();
+    refetch();
+  };
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input value={username} onChange={e => setUsername(e.target.value)} type='text' />
+        <input value={age} onChange={e => setAge(e.target.value)} type='number' />
+        <div className='btns'>
+          <button onClick={e => addUser(e)}>Создать</button>
+          <button onClick={e => getAll(e)}>Получить</button>
+        </div>
+      </form>
+      <div>
+        {users.map(user => (
+          <div key={user.id} className='user'>
+            {user.id}. {user.username} {user.age}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
